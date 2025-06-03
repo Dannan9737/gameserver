7e80a35868bdb92cfac948f9fb006d1ba5416bce
Dannan9737 <dannan9737@gmail.com>
2025-06-03 11:19:17 +0800

    server upload

--- server.js
+++ server.js
@@ -6,12 +6,32 @@
 var express = require("express");
 var server = express();
 
+var bodyParser = require("body-parser");	
+
 server.use(express.static(__dirname+"/public")); //static file
+server.use(bodyParser.urlencoded({extended:true})); //parse urlencoded data
+server.use(bodyParser.json()); //parse json data
+
+const Datastore = require('nedb-promises')
+let GameDB = Datastore.create(__dirname+'/game.db')
 
 //server.get("/", function(req,res){ //web ropt
 //    res.send("Hello World!");//回傳固定内容
 //});
-//server,get("/md", function(req,res){ //other pages
-//    res.send("req/md");
-//});
+server,get("/score", function(req,res){ //other pages
+	console.log(req.query);
+     res.send("req /md");
+ });
+
+ server.post("/rank", (req,res)=>{
+
+ })
+  server.post("/postscore", (req,res)=>{
+	 console.log(req.body);
+	 //save to db
+	 GameDB.insert(req.body);
+	 //find and sort and limit
+	 res.send([{name:"MD", rank:1},{name:"Jhon",rank:2}]);
+  });
+
 server.listen(80);
