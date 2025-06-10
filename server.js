382ba8373cc4a9b798ac7127b731228e21bd0dd3
Dannan9737 <Dannan9737@gmail.com>
2025-06-10 11:21:22 +0800

    upload again

--- server.js
+++ server.js
@@ -1,37 +1,50 @@
-//var http = require('http');
-//http.createServer(function(request, response){
-	//response.write("Hello World!");
-	//response.end(); //close connection
-//}).listen(80); //http port
+// var http = require('http');
+// http.createServer(function(request, response){
+// 	response.write("Hello World!");
+// 	response.end(); //close connection
+// }).listen(80); //http port
 var express = require("express");
 var server = express();
 
-var bodyParser = require("body-parser");	
+var bodyParser = require("body-parser");
 
-server.use(express.static(__dirname+"/public")); //static file
-server.use(bodyParser.urlencoded({extended:true})); //parse urlencoded data
-server.use(bodyParser.json()); //parse json data
+
+
+server.use(express.static(__dirname + "/public"));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 const Datastore = require('nedb-promises')
-let GameDB = Datastore.create(__dirname+'/game.db')
+let GameDB = Datastore.create(__dirname + '/game.db')
 
-//server.get("/", function(req,res){ //web ropt
-//    res.send("Hello World!");//回傳固定内容
-//});
-server,get("/score", function(req,res){ //other pages
-	console.log(req.query);
-     res.send("req /md");
- });
+// server.get("/", function (req, res) { //web root
+//     res.send("Hello, World!"); //回傳固定內容
+// });
+server.get("/score", function (req, res) { //other pages
+   //url?user=md&score=1000
+   console.log(req.query);
+   res.send("req /md");
+});
 
- server.post("/rank", (req,res)=>{
 
- })
-  server.post("/postscore", (req,res)=>{
-	 console.log(req.body);
-	 //save to db
-	 GameDB.insert(req.body);
-	 //find and sort and limit
-	 res.send([{name:"MD", rank:1},{name:"Jhon",rank:2}]);
-  });
+server.post("/rank", (req, res) => {
+   GameDB.find({}, { _id: 0 }).sort({ "score": -1 }).limit(3).then((docs) => {
+      if (docs != null) {
+         res.send(docs);
+      }
+   })
+})
+server.post("/postscore", (req, res) => {
+   console.log(req.body);
+   //save to db
+   GameDB.insert(req.body).then(doc => {
+      //find and sort and limit
+      GameDB.find({}, { _id: 0 }).sort({ "score": -1 }).limit(3).then((docs) => {
+         if (docs != null) {
+            res.send(docs);
+         }
+      })
+   });
+});
 
 server.listen(80);
